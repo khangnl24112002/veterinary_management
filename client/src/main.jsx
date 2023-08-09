3a02ce5678c3d865263364d182c694f7b0dce70b
20120302 <khangnl24112002@gmail.com>
2023-08-09 22:44:23 +0700

    Add persistStore to save redux state in localStorage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "./store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store.js";
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
