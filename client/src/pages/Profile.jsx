b1116fa2c1213dee30ca89448e9a9ee7c82abd78
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2023-08-13 18:50:22 +0700

    Add new UI for profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
+
 const ProfilePage = () => {
+  const initialProfile = {
+    name: "John Doe",
+    age: 30,
+    email: "johndoe@example.com",
+    address: "123 Main St, City",
+  };
+
+  const [profile, setProfile] = useState(initialProfile);
+  const [editing, setEditing] = useState(false);
+  const [isChanged, setIsChanged] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setProfile((prevProfile) => ({
+      ...prevProfile,
+      [name]: value,
+    }));
+    setIsChanged(true);
+  };
+
+  const handleSaveClick = () => {
+    // Simulate saving changes
+    // In a real scenario, you would perform API requests or data updates here
+    setIsSuccess(true);
+    setIsChanged(false);
+  };
+
   return (
-    <div className=" w-[100%]">
-      {/**Title Here */}
-      <h1 className="mt-0">Profile Page</h1>
+    <div className="w-[100%]">
+      <h1 className="mt-0 text-3xl font-semibold">Profile Page</h1>
       <div className="flex items-center h-screen bg-gray-100">
-        {/* Phần bên trái */}
         <div className="flex-none w-1/4 p-8 bg-white shadow-md">
           <img
             src="https://picsum.photos/300"
@@ -12,22 +44,89 @@ const ProfilePage = () => {
             className="w-32 h-32 rounded-full mx-auto"
           />
         </div>
-
-        {/* Phần bên phải */}
         <div className="flex-grow p-8 bg-white shadow-md">
           <h2 className="text-2xl font-semibold mb-4">Profile</h2>
-          <p>
-            <span className="font-semibold">Họ và Tên:</span> John Doe
-          </p>
-          <p>
-            <span className="font-semibold">Tuổi:</span> 30
-          </p>
-          <p>
-            <span className="font-semibold">Email:</span> johndoe@example.com
-          </p>
-          <p>
-            <span className="font-semibold">Địa Chỉ:</span> 123 Main St, City
-          </p>
+          <form className="space-y-4">
+            <div className="flex items-center">
+              <span className="font-semibold w-1/4">Họ và Tên:</span>
+              {editing ? (
+                <input
+                  type="text"
+                  name="name"
+                  value={profile.name}
+                  onChange={handleInputChange}
+                  className="w-3/4 p-2 border rounded focus:outline-none focus:border-blue-500"
+                />
+              ) : (
+                <p className="w-3/4">{profile.name}</p>
+              )}
+            </div>
+            <div className="flex items-center">
+              <span className="font-semibold w-1/4">Tuổi:</span>
+              {editing ? (
+                <input
+                  type="number"
+                  name="age"
+                  value={profile.age}
+                  onChange={handleInputChange}
+                  className="w-3/4 p-2 border rounded focus:outline-none focus:border-blue-500"
+                />
+              ) : (
+                <p className="w-3/4">{profile.age}</p>
+              )}
+            </div>
+            <div className="flex items-center">
+              <span className="font-semibold w-1/4">Email:</span>
+              {editing ? (
+                <input
+                  type="email"
+                  name="email"
+                  value={profile.email}
+                  onChange={handleInputChange}
+                  className="w-3/4 p-2 border rounded focus:outline-none focus:border-blue-500"
+                />
+              ) : (
+                <p className="w-3/4">{profile.email}</p>
+              )}
+            </div>
+            <div className="flex items-center">
+              <span className="font-semibold w-1/4">Địa Chỉ:</span>
+              {editing ? (
+                <input
+                  type="text"
+                  name="address"
+                  value={profile.address}
+                  onChange={handleInputChange}
+                  className="w-3/4 p-2 border rounded focus:outline-none focus:border-blue-500"
+                />
+              ) : (
+                <p className="w-3/4">{profile.address}</p>
+              )}
+            </div>
+            {editing ? (
+              <div className="flex space-x-4">
+                <button
+                  className="flex-grow bg-blue-500 text-white py-2 rounded hover:bg-blue-700"
+                  onClick={handleSaveClick}
+                  disabled={!isChanged}
+                >
+                  Save
+                </button>
+                {isSuccess && (
+                  <p className="flex-none text-green-600 self-center">
+                    Cập nhật thành công!
+                  </p>
+                )}
+              </div>
+            ) : (
+              <button
+                className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+                onClick={handleEditClick}
+              >
+                Edit
+              </button>
+            )}
+          </form>
         </div>
       </div>
     </div>
