dd32e4ea2990116204ce2e542b1b0b63b326c633
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2023-08-17 16:18:07 +0700

    Add cloudinary config

---
+++ server/upload/upload.js
@@ -0,0 +1,7 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "daexxhimb",
+  api_key: "928598793819699",
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
