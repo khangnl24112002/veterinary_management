6edcde3322b6894993fde6102a7bf6db19204cdc
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2023-09-04 17:17:56 +0700

    Changing adding new account btn

--- client/src/pages/ManageAccounts/ManageAccounts.jsx
+++ client/src/pages/ManageAccounts/ManageAccounts.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const ManageAccounts = () => {
   return (
-    <div className="">
+    <div className="m-5">
       <h1 className="mt-0 pl-5">Manage Accounts</h1>
       <Outlet />
     </div>
--- client/src/pages/ManageAccounts/ViewAccounts.jsx
+++ client/src/pages/ManageAccounts/ViewAccounts.jsx
@@ -19,7 +19,12 @@ const ViewAccounts = () => {
   return (
     <div>
       <Table data={accounts} />
-      <Link to="addNewAccount">Add new account...</Link>
+      <Link
+        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none mb-4 mt-4"
+        to="addNewAccount"
+      >
+        Add new account
+      </Link>
     </div>
   );
 };
