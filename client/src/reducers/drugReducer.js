601a47141ddb3c62d26f4aea0261f2a96a4e2308
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2023-09-11 09:33:59 +0700

    Config drug reducer

---
+++ client/src/reducers/drugReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  drug: {},
+};
+
+// Tao reducer de quan ly trang thai cua User
+const drugReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default drugReducer;
--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import userReducer from "./userReducer";
 import accountReducer from "./accountReducer";
+import drugReducer from "./drugReducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   user: userReducer,
   account: accountReducer,
+  drug: drugReducer,
 });
 
 export default rootReducer;
