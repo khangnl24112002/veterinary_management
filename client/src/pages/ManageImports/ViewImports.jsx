1c0c891faadae5dcf1a56a4b5738f70d90252c77
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2023-09-22 14:56:10 +0700

    Adding viewing imports list

--- client/src/pages/ManageImports/ViewImports.jsx
+++ client/src/pages/ManageImports/ViewImports.jsx
@@ -1,34 +1,44 @@
 import { Link } from "react-router-dom";
+import ImportTable from "../../components/ImportTable/ImportTable";
+import { useEffect, useState } from "react";
+import { getAllImports } from "../../services/import.services";
 
 const ViewImports = () => {
+  const [data, setData] = useState();
+  useEffect(() => {
+    const fetchAllData = async () => {
+      const response = await getAllImports();
+      if (response.success) {
+        const responseData = response.data;
+        responseData.map((data) => {
+          Reflect.deleteProperty(data, "createdAt");
+          Reflect.deleteProperty(data, "updatedAt");
+        });
+        setData(responseData);
+      } else {
+        return <p>Cannot get data!</p>;
+      }
+    };
+    fetchAllData();
+  }, []);
+  console.log(data);
   return (
     <div>
-      <button className="my-4">
-        <Link
-          to="addNewImport"
-          className="my-4 px-4 py-2 text-white hover:bg-blue-700 bg-blue-500 rounded-lg"
-        >
-          Add new Import
-        </Link>
-      </button>
-      <table>
-        <thead>
-          <tr>
-            <td>Col1</td>
-            <td>Col2</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Row1</td>
-            <td>Row1</td>
-          </tr>
-          <tr>
-            <td>Row2</td>
-            <td>Row2</td>
-          </tr>
-        </tbody>
-      </table>
+      {data ? (
+        <div>
+          <button className="my-4">
+            <Link
+              to="addNewImport"
+              className="my-4 px-4 py-2 text-white hover:bg-blue-700 bg-blue-500 rounded-lg"
+            >
+              Add new Import
+            </Link>
+          </button>
+          <ImportTable data={data} />
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
