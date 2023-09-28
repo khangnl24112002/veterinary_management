876361e76f03eb208dc61d9f20a02887c6d3d863
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2023-09-28 16:06:44 +0700

    Fixing navbar

--- client/src/pages/ManageAppointmentCustomer/ManageAppointments.jsx
+++ client/src/pages/ManageAppointmentCustomer/ManageAppointments.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 
 const ManageAppointments = () => {
-  return (
-    <div className="m-5">
-      <h1 className="mt-0 mb-10 pl-5">Manage Appointment</h1>
-      <Outlet />
-    </div>
-  );
+  const userInfo = JSON.parse(localStorage.getItem("account"));
+  if (userInfo?.role === 2) {
+    return (
+      <div className="m-5">
+        <h1 className="mt-0 mb-10 pl-5">Manage Appointments</h1>
+        <Outlet />
+      </div>
+    );
+  } else {
+    window.location.replace("/unauthorized");
+  }
 };
 
 export default ManageAppointments;
